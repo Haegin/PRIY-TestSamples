76034810b1229a97a8ab72c767ae2688ae03b6d1
Harry Mills <harry@haeg.in>
2012-02-07 16:09:46 +0000

    Adds a script to recompile everything

---
+++ repackage.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+# use log files
+[[ -e errors.log ]] && rm errors.log
+[[ -e messags.log ]] && rm messags.log
+touch errors.log
+touch messages.log
+
+# cd in to each project dir
+for dir in *; do
+    if [[ -d ${dir} ]]; then
+        cd "${dir}"
+        echo "Building ${dir}"
+        mvn package >> ../messages.log 2>> ../errors.log
+        echo "Done\n"
+        cd ../
+    fi
+done
